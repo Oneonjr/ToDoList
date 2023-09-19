b1e2e59b3d4cfd55923ad16fcdffb6905ab82069
Oneonjr <oneonjunior@outlook.com>
2023-09-19 14:23:50 -0300

    criando O U do CRUD

--- index.php
+++ index.php
@@ -28,6 +28,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .editar-form{
+            display: none;
+        }
+    </style>
     <link rel="stylesheet" href="./assets/style.css">
 </head>
 <body>
@@ -55,6 +60,28 @@
                     $bd->exec("DELETE FROM tasks WHERE id=$id");
                     echo 'Deletado com sucesso o id: '.$id;
                 }
+                
+                //Atualizando Dados.
+                if (isset($_POST['tituloEdi'])) {
+                    $id = $_POST['id']; // Obtém o ID do registro a ser atualizado
+                    $tituloEdi = $_POST['tituloEdi'];
+                    $descriEdi = $_POST['descriEdi'];
+                
+                    // Execute a atualização no banco de dados
+                    $stmt = $bd->prepare("UPDATE tasks SET title = :title, description = :description WHERE id = :id");
+                    $stmt->bindParam(':title', $tituloEdi);
+                    $stmt->bindParam(':description', $descriEdi);
+                    $stmt->bindParam(':id', $id);
+                
+                    if ($stmt->execute()) {
+                        echo 'Dados atualizados com sucesso!';
+                    } else {
+                        echo 'Erro ao atualizar dados.';
+                    }
+                }
+                
+                
+
 
                 //Mostrando a lista dos dados.
 
@@ -64,23 +91,48 @@
 
                 $info = $sql->fetchAll();
 
-                for ($i=0; $i < count($info); $i++) { 
+                foreach ($info as $key => $value) { 
                     echo '<hr>';
-                    echo 'Título: '.$info[$i]['title'];
                     echo '<br>';
-                    echo 'Conteudo: '.$info[$i]['description'];
+                    echo 'Id: '.$value['id'];
                     echo '<br>';
-                    echo 'Status: '.$info[$i]['completed'];
+                    echo 'Título: '.$value['title'];
                     echo '<br>';
-                    echo '<a href="?delete='.$info[$i]['id'].'">Apagar</a>';
+                    echo 'Conteudo: '.$value['description'];
+                    echo '<br>';
+                    echo 'Status: '.$value['completed'];
+                    echo '<br>';
+                    echo '<a href="?delete='.$value['id'].'">Apagar</a>';
+                    echo ' | ';
+                    echo '<a href="" class="editar-link">Editar</a>';
+                    echo '<form class="editar-form" method="POST">
+                            <input type="hidden" name"id" value="' . $value['id'] . '"></input>
+                            <input type="text" name="tituloEdi" id="campo1" placeholder="Título">
+                            <input type="text" name="descriEdi" id="campo1" placeholder="Descrição">
+                            <button type="submit" name="submit">Enviar</button>
+                        </form>';
                     echo '<hr>';
                 } 
             ?>
+
+
+
             </div>
         </section>
     </main>
     
     <script>
+        const editarLink = document.querySelectorAll(".editar-link");
+        const editarForm = document.querySelectorAll(".editar-form");
+
+        editarLink.forEach((link,index) => {
+            link.addEventListener("click",function(event){
+            event.preventDefault();
+
+            editarForm[index].style.display = editarForm[index].style.display === "none" ? "block" : "none";
+            
+        });
+        });
     </script>
 </body>
 </html>
